982a805808f670caf92cabdd2ae1362fcc5398d7
Jonathan Zarecki <yonilx@gmail.com>
2020-03-19 11:27:13 +0200

    small change to test-real-data

--- edge_prop/tests/test_real_data.py
+++ edge_prop/tests/test_real_data.py
@@ -9,23 +9,20 @@ from edge_prop.models.dense_edge_propagation import DenseEdgeProp
 
 
 class TestDenseEdgeProp(TestCase):
-
     @classmethod
     def setUpClass(cls) -> None:
         path = DATASET2PATH['slashdot']
-        cls.graph, cls.y_true, cls.test_indices = DataLoader(path, test_size=0.9).load_data(1_000)
+        cls.graph, cls.y_true, cls.test_indices = DataLoader(path, test_size=0.2).load_data(1_000)
         cls.y_test = cls.y_true[cls.test_indices]
 
-
-    def test_predict(self):
-        baseline = DenseBasline(self.graph.y_attr, max_iter=1000, alpha=0.8, tol=0.1)
+    def test_baseline(self):
+        baseline = DenseBasline(self.graph.y_attr, max_iter=1000, alpha=0.8, tol=1e-3)
         baseline.fit(self.graph)
         y_pred = baseline.predict()[self.test_indices]
         print(f"Accuracy: {accuracy_score(self.y_test, y_pred)}")
 
-
-    def test_predict2(self):
-        edge_prop = DenseEdgeProp(self.graph.y_attr, max_iter=1000, alpha=0.8, tol=0.1)
+    def test_edgeprop(self):
+        edge_prop = DenseEdgeProp(self.graph.y_attr, max_iter=1000, alpha=0.8, tol=1e-3)
         edge_prop.fit(self.graph)
 
         y_pred = edge_prop.predict()[self.test_indices]
